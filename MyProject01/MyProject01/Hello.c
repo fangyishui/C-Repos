1cb75b9e77570fc791af8c24daa5ef37d5bb97b3
y <y@DESKTOP-EK3RHH6>
2021-12-03 23:27:03 +0800

    添加项目文件。

---
+++ MyProject01/MyProject01/Hello.c
@@ -0,0 +1,14 @@
+//开发项目
+
+//引入头文件 
+#include <stdio.h>
+
+//这是一个main函数 是程序的执行入口
+void main() {
+
+	//1. printf是一共函数 需要 头文件才能使用
+	//2  printf实在 <stdio.h> 需要引入该头文件
+	printf("Hello World!!!");
+
+	getchar();//让窗口停留
+}
